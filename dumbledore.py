f29f7189ec07b64c5147e112795f69abf8c46062
Rose <71735227+DRBriseno@users.noreply.github.com>
2021-02-26 17:55:36 -0800

    Add files via upload
    dumbledore.py

---
+++ dumbledore.py
@@ -0,0 +1,8 @@
+from hero import Hero
+
+if __name__ == "__main__":
+    # If you run this file from the terminal
+    # this block is executed.
+     hero_1 = Hero("Dumbledore", 200)
+     print(hero_1.name)
+     print(hero_1.current_health)  
