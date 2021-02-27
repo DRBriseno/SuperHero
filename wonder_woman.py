b89b7ec7cf87556b54d6c8410d55f54b50ced4ae
Rose <71735227+DRBriseno@users.noreply.github.com>
2021-02-26 17:56:22 -0800

    Add files via upload
    wonder_woman.py

---
+++ wonder_woman.py
@@ -0,0 +1,8 @@
+from hero import Hero
+
+if __name__ == "__main__":
+    # If you run this file from the terminal
+    # this block is executed.
+     hero_2 = Hero("Wonder Woman", 200)
+     print(hero_2.name)
+     print(hero_2.current_health)  
